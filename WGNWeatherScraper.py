faecdf0426230f802673318337d3317fca66c8c0
Johnny Heintz <jorune00@gmail.com>
2023-11-07 13:47:32 -0600

    Temp read from KILAURO99 and WGN Weather

--- WGNWeatherScraper.py
+++ WGNWeatherScraper.py
@@ -1,3 +1,21 @@
 #!/usr/bin/env python 
-# jorune00 -- 2023-11-06 14:25:44
+# jorune00 - jorune.dev -- 2023-11-06 14:25:44
 # Web scraping WGN Weather page for local weather data
+
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://wgntv.com/weather/'
+
+page = requests.get(url)
+
+if page.status_code == 200:
+    soup = BeautifulSoup(page.content, 'lxml')
+    temp_element = soup.select_one('div.weather-tabs__panel:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > span:nth-child(2)')
+    if temp_element:
+        tempurature = temp_element.text
+        print(f"WGN Naperville Temp: {tempurature}")
+    else:
+        print("Temperature element not found.")
+else:
+    print(f"Failed to retrieve the webpage, status code: {page.status_code}")
